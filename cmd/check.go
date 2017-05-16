b477fb6e1d90592c72f23e96081d1106faf90e54
sven <sven@so36.net>
2017-05-16 13:02:38 +0200

    Start with basic, dependecies checks

---
+++ cmd/check.go
@@ -0,0 +1,28 @@
+package main
+ 
+import (
+   "fmt"
+   "os"
+)
+ 
+func main() {
+
+	// check if docker is installed
+	// print message telling this
+	fmt.Println("Checking for dependecies")
+
+   // get FileInfo structure describing file
+   _, err := os.Stat("/usr/bin/docker")
+ 
+   // check if there is an error
+   if err != nil {
+ 
+      // check if error is file does not exist
+      if os.IsNotExist(err) {
+         fmt.Println("Can't find docker")
+      }
+ 
+   } else {
+    fmt.Println("Yeah, looks good !")
+   }
+}
