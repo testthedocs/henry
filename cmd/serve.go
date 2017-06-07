0c5e84c48762bb81c920d7aa570fcabdb77ab395
sven <sven@so36.net>
2017-06-07 11:51:23 +0200

    Add: serve function

---
+++ cmd/serve.go
@@ -0,0 +1,59 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"os"
+    "os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Serves documentation and rebuild if a change is detected",
+	Long: `Browse docs locally and rebuild automatically is a change it detected.
+
+	Note: This is currently alpha status and only tested on Linux.
+	Because if the usage of "net=host" this is considerd insecure!`,
+	Run: func(cmd *cobra.Command, args []string) {
+		serveHTML()
+	},
+}
+
+
+func serveHTML() {
+	cmdStr := "docker run -it --net=host -v `pwd`:/build/docs pdbab serve"
+	cmd := exec.Command("bash", "-c", cmdStr)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+}
+
+func init() {
+	RootCmd.AddCommand(serveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
