5dd9e0fe95fcdd3156132d6f1fe6284ea9122fb0
sven <sven@so36.net>
2017-06-20 17:25:58 +0200

    Better version

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import "github.com/testthedocs/henry/cmd"
 
 var (
         // VERSION is set during build
-            VERSION = "0.0.1-alpha"
+            VERSION = "0.0.1-alpha.1"
         )
 
 func main() {
