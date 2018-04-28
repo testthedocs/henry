a6f228a5da0ab68640e5db5fa804d2700a547d5e
sven <sven@so36.net>
2018-04-28 15:41:10 +0200

    meta: Add netlify

--- .circleci/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error
-
-zip -r website.zip ~/sphinx/html/_build/html
-
-curl -H "Content-Type: application/zip" \
-     -H "Authorization: Bearer $NETLIFYKEY" \
-     --data-binary "@website.zip" \
-     https://api.netlify.com/api/v1/sites/$API/deploys
